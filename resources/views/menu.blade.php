a93b8c47c5dc6ee6925efdce54cbc5710d04c859
Alex Khayev <alexander.khayev@gmail.com>
2020-10-13 01:08:44 +0400

    finish Lesson03

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,3 +4,7 @@
         <a class="p-2 text-dark" href="{{ route('admin.home') }}">Admin lk</a>
         <a class="p-2 text-dark" href="{{ route('news.home') }}">All news</a>
         <a class="p-2 text-dark" href="{{ route('vue') }}">Vue</a>
+
+        <!--
+        // TODO: сделать админское меню выпадающим списком
+        -->
