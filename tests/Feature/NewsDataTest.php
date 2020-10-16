dae640544a7a1760682fd91a962f9ee119cd6b87
Alex Khayev <alexander.khayev@gmail.com>
2020-10-16 12:37:02 +0400

    add any tests, probally :)

---
+++ tests/Feature/NewsDataTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\News;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class NewsDataTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testNews()
+    {
+        $this->assertTrue(is_array(News::factory()->getAllNews()));
+    }
+
+    public function testStructure()
+    {
+        $this->assertTrue($this->is_correct_structure(News::factory()->getAllNews()));
+    }
+
+    public function testRoutes() {
+        $response = $this->get(route('home'));
+        $response->assertStatus(200);
+    }
+
+    public function is_correct_structure($object) {
+        return is_array($object) &&
+            !is_null($object[0]);
+    }
+}
